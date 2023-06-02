bb15be4e88c97f40aa9882f981786121087051d0
Admin <Admin@WIN-5I7J1UL4AE7>
2023-06-02 12:11:44 +0300

    Добавьте файлы проекта.

---
+++ 02.06dz.versta/02.06dz.versta.cpp
@@ -0,0 +1,23 @@
+﻿
+
+#include <iostream>
+#include<Windows.h>
+using namespace std;
+
+
+int main()
+{
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	cout << "Обчислення верст та кiлометрiв"<<endl;
+	double versta = 1.067;
+	double kilometru;
+	double kilkist;
+	cout << "Введiть кiльскiсть верст: "<<endl;
+	cin >> kilkist;
+	kilometru = kilkist / versta;
+	cout << "Кiлькiсть "<<kilometru<<" з " << kilkist << " верст";
+
+}
+
+
